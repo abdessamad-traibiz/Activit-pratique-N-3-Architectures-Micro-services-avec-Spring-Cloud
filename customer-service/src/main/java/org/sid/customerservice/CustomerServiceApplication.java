e51d3bfe86cffa6cfa606e01366a70cccab3793f
Abdessamad Traibiz <abdessamad.traibiz@gmail.com>
2022-11-06 23:28:26 +0100

    Update

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -6,16 +6,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @SpringBootApplication
-public class CustomerServiceApplication {
+public class CustomerServiceApplication   {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
 	@Bean
-	CommandLineRunner start(CustomerRepository customerRepository){
+	CommandLineRunner start(CustomerRepository customerRepository  ){
 		return  args -> {
 			customerRepository.save(new Customer(null,"Abdessamad","abdessamad@gmail.com"));
 			customerRepository.save(new Customer(null,"Amine","amine@gmail.com"));
--- customer-service/src/main/java/org/sid/customerservice/entities/Customer.java
+++ customer-service/src/main/java/org/sid/customerservice/entities/Customer.java
@@ -1,5 +1,6 @@
 package org.sid.customerservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
